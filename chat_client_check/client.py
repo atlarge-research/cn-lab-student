2b2f690b69a82fbfd11500cce4e1e681fa32b0ef
YonusD <45369631+YonusD@users.noreply.github.com>
2024-04-08 01:07:20 +0200

    Corrected welcome message
    The autotest checks for the welcome message with "your". However, this prebuilt file has written "you", making it a typo. 
    
    VUnet ID: 2808579

--- chat_client_check/client.py
+++ chat_client_check/client.py
@@ -1,5 +1,5 @@
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5378
 
-print('Welcome to Chat Client. Enter you login:')
+print('Welcome to Chat Client. Enter your login:')
 # Please put your code in this file
